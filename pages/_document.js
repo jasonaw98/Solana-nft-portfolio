601ed4b0a71e3bc4c6ed65d669939e31aaeb7096
Jason <jason.aw@attspace.ventures>
2023-05-31 23:15:48 +0800

    check

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,29 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7966611751536031"
-     crossorigin="anonymous"></script>
+      <script
+        async
+        custom-element="amp-ad"
+        src="https://cdn.ampproject.org/v0/amp-ad-0.1.js"
+      ></script>
+      <amp-ad
+        width="100vw"
+        height="320"
+        type="adsense"
+        data-ad-client="ca-pub-7966611751536031"
+        data-ad-slot="2750679352"
+        data-auto-format="rspv"
+        data-full-width=""
+      >
+        <div overflow=""></div>
+      </amp-ad>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
