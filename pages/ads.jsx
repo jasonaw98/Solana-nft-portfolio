4ffd72be053195965e84d2ff30cbe47a578798b8
Jason <jason.aw@attspace.ventures>
2023-06-02 00:10:47 +0800

    changes

--- pages/ads.jsx
+++ pages/ads.jsx
@@ -1,27 +1,31 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import React from "react";
+import Image from "next/image";
 
-export default function Document() {
+const Ads = () => {
   return (
-    <Html lang="en">
-      <Head>
-        {/* <script
+    <div>
+ <script
         type="text/javascript"
         src="https://udbaa.com/bnr.php?section=General&pub=782563&format=300x250&ga=g"
       ></script>
       <noscript>
         <a href="https://yllix.com/publishers/782563" target="_blank">
+          {/* <Image
+           src="//ylx-aff.advertica-cdn.com/pub/300x250.png"
+           style={{ border: 'none', margin: 0, padding: 0, verticalAlign: 'baseline' }}
+           alt="ylliX - Online Advertising Network"
+           width={500}
+           height={500}
+          /> */}
           <img
             src="//ylx-aff.advertica-cdn.com/pub/300x250.png"
             style={{ border: 'none', margin: 0, padding: 0, verticalAlign: 'baseline' }}
             alt="ylliX - Online Advertising Network"
           />
         </a>
-      </noscript> */}
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+        </noscript>
+    </div>
   );
-}
+};
+
+export default Ads;
