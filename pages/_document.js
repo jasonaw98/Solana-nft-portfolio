e5c362151ab91c9bae7c12cd260e64cd1e3172f8
Jason <jason.aw@attspace.ventures>
2023-06-01 18:56:48 +0800

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <meta name="google-site-verification" content="YbP5-N7K62A7Ta9HpGrkKbIgxWV1hPS82aAcZ02xoq4" />
-      <noscript>&lt;a href="https://yllix.com/publishers/782563" target="_blank"&gt;&lt;img src="//ylx-aff.advertica-cdn.com/pub/160x600.png" style="border:none;margin:0;padding:0;vertical-align:baseline;" alt="ylliX - Online Advertising Network" /&gt;&lt;/a&gt;</noscript>
-        </Head>
+        <noscript>
+          &lt;a href="https://yllix.com/publishers/782563"
+          target="_blank"&gt;&lt;img
+          src="//ylx-aff.advertica-cdn.com/pub/160x600.png"
+          style="border:none;margin:0;padding:0;vertical-align:baseline;"
+          alt="ylliX - Online Advertising Network" /&gt;&lt;/a&gt;
+        </noscript>
+      </Head>
       <body>
         <Main />
         <NextScript />
