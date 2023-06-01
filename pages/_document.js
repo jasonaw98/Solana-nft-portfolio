861d3ac627a85a0c05d35f97eb08497f00c0a17d
Jason <jason.aw@attspace.ventures>
2023-06-01 18:53:08 +0800

    link

--- pages/_document.js
+++ pages/_document.js
@@ -5,22 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
       <meta name="google-site-verification" content="YbP5-N7K62A7Ta9HpGrkKbIgxWV1hPS82aAcZ02xoq4" />
-      <script
-        async
-        custom-element="amp-ad"
-        src="https://cdn.ampproject.org/v0/amp-ad-0.1.js"
-        ></script>
-      <amp-ad
-        width="100vw"
-        height="320"
-        type="adsense"
-        data-ad-client="ca-pub-7966611751536031"
-        data-ad-slot="2750679352"
-        data-auto-format="rspv"
-        data-full-width=""
-        >
-        <div overflow=""></div>
-      </amp-ad>
+      <noscript>&lt;a href="https://yllix.com/publishers/782563" target="_blank"&gt;&lt;img src="//ylx-aff.advertica-cdn.com/pub/160x600.png" style="border:none;margin:0;padding:0;vertical-align:baseline;" alt="ylliX - Online Advertising Network" /&gt;&lt;/a&gt;</noscript>
         </Head>
       <body>
         <Main />
