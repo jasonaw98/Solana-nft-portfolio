3d45d3abee0d34333cf3e77233c308a321acc631
Jason <jason.aw@attspace.ventures>
2023-06-01 16:17:55 +0800

    vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
 import '@/styles/globals.css'
 import MintsByOwnerPage from './mintByOwner'
+import { Analytics } from '@vercel/analytics/react';
+
 
 export default function App() {
   return <div>
     <MintsByOwnerPage />
+    <Analytics />
   </div>
 }
