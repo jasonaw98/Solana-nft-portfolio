afcb766465e37082d911ccc0213c3c11132c353b
Jason <jason.aw@attspace.ventures>
2023-06-02 01:06:38 +0800

    remove in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,10 @@
 import '@/styles/globals.css'
 import MintsByOwnerPage from './mintByOwner'
 import { Analytics } from '@vercel/analytics/react';
-import Ads from './ads';
 
 
 export default function App() {
   return <div>
-    {/* <Ads/> */}
     <MintsByOwnerPage />
     <Analytics />
   </div>
